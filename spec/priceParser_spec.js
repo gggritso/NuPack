8773b6ae1040d0094888296dd92c772b4d09415b
George Gritsouk <george.gritsouk@gmail.com>
2015-01-23 15:36:52 -0500

    fix: Poor whitespace style.

--- spec/priceParser_spec.js
+++ spec/priceParser_spec.js
@@ -3,11 +3,13 @@ var parse = require( '../src/priceParser.js' ).parse;
 describe( 'PriceParser test suite', function() {
 
   it( 'should account for dollar sign', function() {
+
     expect( parse( '$1200' ) ).toEqual( 1200 );
     expect( parse( '1200' ) ).toEqual( 1200 );
   });
 
   it( 'should work with decimals', function() {
+
     expect( parse( '$1200.23' ) ).toEqual( 1200.23 );
     expect( parse( '1200.00' ) ).toEqual( 1200 );
   });
--- src/priceParser.js
+++ src/priceParser.js
@@ -1,6 +1,7 @@
 var priceRegExp = /^\$?([\d\,\.]+)$/;
 
 module.exports.parse = function( priceString ) {
+
   var numberString = priceRegExp.exec( priceString )[ 1 ].replace( /,/g, '' );
   return parseFloat( numberString, 10 );
 };
