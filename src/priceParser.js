a2abb7c2d681be81e344caf8f4777928d0ad8b73
George Gritsouk <george.gritsouk@gmail.com>
2015-01-23 12:04:41 -0500

    add: PriceParser module.

---
+++ src/priceParser.js
@@ -0,0 +1,6 @@
+var priceRegExp = /^\$?([\d\,\.]+)$/;
+
+module.exports.parse = function( priceString ) {
+  var numberString = priceRegExp.exec( priceString )[ 1 ].replace( /,/g, '' );
+  return parseFloat( numberString, 10 );
+};
