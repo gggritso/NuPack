f46359f59ab8127b4502b5a89d04564403705df3
George Gritsouk <george.gritsouk@gmail.com>
2015-01-23 12:26:22 -0500

    add: Initial NuPack module implementation.

--- spec/nupack_spec.js
+++ spec/nupack_spec.js
@@ -1,9 +1,41 @@
-var nupack = require( '../src/nupack.js' );
+var getProjectCostEstimate = require( '../src/nupack.js' ).getProjectCostEstimate;
 
-describe( 'NuPack test suite', function() {
+describe( 'NuPack error suite', function() {
 
-  it( 'should return foo', function() {
-    expect( nupack.foo() ).toBe( 'foo' );
+  it( 'should error on no base price', function() {
+
+      expect( function() {
+        getProjectCostEstimate();
+      }).toThrowError( 'no price specified' );
+
+  });
+
+  it( 'should error on no malformed price', function() {
+
+    expect( function() {
+      getProjectCostEstimate({ hey: 'hey' });
+    }).toThrowError( 'basePrice should be float or string' );
+
+  });
+
+});
+
+describe( 'NuPack success suite', function() {
+
+  it( 'should calculate food markup successfully', function() {
+    expect( getProjectCostEstimate( '$1299.99', 'food', 3 ) ).toEqual( 1591.58 );
+  });
+
+  it( 'should calculate food markup successfully from float', function() {
+    expect( getProjectCostEstimate( 1299.99, 'food', 3 ) ).toEqual( 1591.58 );
+  });
+
+  it( 'should calculate drug markup successfully', function() {
+    expect( getProjectCostEstimate( '$5432.00', 'drugs', 1 ) ).toEqual( 6199.81 );
+  });
+
+  it( 'should calculate no category markup successfully', function() {
+    expect( getProjectCostEstimate( '$12456.95', 'books', 4 ) ).toEqual( 13707.63 );
   });
 
 });
--- src/nupack.js
+++ src/nupack.js
@@ -1,3 +1,51 @@
-module.exports.foo = function foo() {
-  return 'foo';
+var parsePrice = require( './priceParser.js' ).parse;
+
+// CONSTANTS
+FLAT_MARKUP = 0.05;
+PERSONNEL_MARKUP = 0.012;
+CATEGORY_MARKUPS = {
+  '': 0,
+  drugs: 0.075,
+  food: 0.13,
+  electronics: 0.02
 };
+
+// PRIVATE FUNCTIONS
+function getProjectCostEstimate( basePrice, categoryName, requiredPersonnelCount ) {
+
+  // check parameters on API-facing function
+  if ( !basePrice ) throw new Error( 'no price specified' );
+
+  if ( typeof basePrice === 'string' ) {
+    basePrice = parsePrice( basePrice );
+  } else if ( typeof basePrice !== 'number' ) {
+    console.log( typeof basePrice );
+    throw new Error( 'basePrice should be float or string' );
+  }
+
+  if ( !categoryName ) categoryName = '';
+  if ( !requiredPersonnelCount ) requiredPersonnelCount = 0;
+
+  basePrice = basePrice + getFlatMarkup( basePrice );
+  var newPrice = basePrice + getPersonnelMarkup( basePrice, requiredPersonnelCount );
+  newPrice += getCategoryMarkup( basePrice, categoryName );
+
+  // FIXME: There are bugs in rounding in JavaScript, MDN has a workaround at
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+  return Math.round( newPrice * 100, 2 ) / 100;
+}
+
+function getFlatMarkup( basePrice ) {
+  return basePrice * FLAT_MARKUP;
+}
+
+function getPersonnelMarkup( basePrice, personnelCount ) {
+  return basePrice * ( PERSONNEL_MARKUP * personnelCount );
+}
+
+function getCategoryMarkup( basePrice, categoryName ) {
+  return basePrice * ( CATEGORY_MARKUPS[ categoryName ] || 0 );
+}
+
+// EXPOSURE
+module.exports.getProjectCostEstimate = getProjectCostEstimate;
